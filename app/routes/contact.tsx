1a9e7964ebf0d49c01e11d67d223bee9094d863d
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-05-08 20:53:48 +0200

    feat: basic form validation

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,6 +1,10 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/cloudflare";
+import { Form, json, useActionData } from '@remix-run/react';
 import clsx from "clsx";
 import { Layout } from "~/components/layout";
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,63 +12,99 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const form = await request.formData();
+  const data = Object.fromEntries(form) as Schema;
+
+  try {
+    schema.parse(data);
+  } catch {
+    return json({ success: false, data  }, { status: 400 });
+  }
+
+  console.log({ data })
+
+  return json({ success: true, data });
+}
+
 export default function Contact() {
+  const data = useActionData<typeof action>();
+  const {
+    register, handleSubmit, formState: { errors, isValid },
+  } = useForm<Schema>({ resolver: zodResolver(schema) });
+
+  if (data?.success === true) {
+    return (
+      <Layout>
+        <p className="text-emerald-600">Form submitted successfully!</p>
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
-      <p className="text-emerald-600">Form submitted successfully!</p>
-
-      <form className="flex flex-col gap-4">
+      <Form
+        className="flex flex-col gap-4"
+        action="/contact"
+        method="post"
+        onSubmit={isValid ? undefined : handleSubmit(() => {})}>
         <div className="grid grid-cols-2 gap-4">
           <div>
-            <label htmlFor="firstName" className="font-medium block mb-1.5">
-              First name
+            <label htmlFor="fullName" className="font-medium block mb-1.5">
+              First name *
             </label>
             <input
               type="text"
               id="fullName"
-              name="fullName"
+              {...register('fullName')}
               className={clsx(
                 "block w-full px-2 py-1.5 text-gray-900 border border-gray-300 bg-gray-50 rounded-sm text-xs focus:border-blue-500",
-                { "border-red-600": true }
+                { "border-red-600": !!errors.fullName?.message }
               )}
             />
-            <p className="text-red-600 mt-1.5">
-              error
-            </p>
+            {errors.fullName?.message && (
+              <p className="text-red-600 mt-1.5">
+                {errors.fullName?.message}
+              </p>
+            )}
           </div>
           <div>
             <label htmlFor="email" className="font-medium block mb-1.5">
-              Email
+              Email *
             </label>
             <input
               type="email"
               id="email"
-              name="email"
+              {...register('email')}
               className={clsx(
                 "block w-full px-2 py-1.5 text-gray-900 border border-gray-300 bg-gray-50 rounded-sm text-xs focus:border-blue-500",
-                { "border-red-600": true }
+                { "border-red-600": !!errors.email?.message }
               )}
             />
-            <p className="text-red-600 mt-1.5">
-              error
-            </p>
+            {errors.email?.message && (
+              <p className="text-red-600 mt-1.5">
+                {errors.email?.message}
+              </p>
+            )}
           </div>
         </div>
         <div>
           <label htmlFor="message" className="font-medium block mb-1.5">
-            Message
+            Message *
           </label>
           <textarea
             id="message"
-            name="message"
+            {...register('message')}
             className={clsx(
               "block w-full px-2 py-1.5 min-h-32 text-gray-900 border border-gray-300 bg-gray-50 rounded-sm text-xs focus:border-blue-500",
-              { "border-red-600": true },
+              { "border-red-600": !!errors.message?.message }
             )}
           />
-          <p className="text-red-600 mt-1.5">
-            error
-          </p>
+          {errors.message?.message && (
+            <p className="text-red-600 mt-1.5">
+              {errors.message?.message}
+            </p>
+          )}
         </div>
         <div>
           <button
@@ -74,7 +114,15 @@ export default function Contact() {
             Submit
           </button>
         </div>
-      </form>
+      </Form>
     </Layout>
   );
 }
+
+const schema = z.object({
+  fullName: z.string().min(4, { message: 'Please enter first name.' }),
+  email: z.string().email({ message: 'Please enter a valid email address.' }),
+  message: z.string().min(48, { message: 'Oops, it seems your message is rather short.' }),
+});
+
+type Schema = z.infer<typeof schema>;
